08af1b2139f12ac7ceb845719bc574dda2443a75
Kolya <murzinov.nikolay@yandex.ru>
2018-10-11 22:12:08 +0300

    za 11 chislo

---
+++ урок 6.2/src/js/Untitled-1.js
@@ -0,0 +1,24 @@
+let button = document.getElementById('start'),
+    budgetValue = document.querySelector('.budget-value'),
+    daybudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalexpensesValue = document.querySelector('.optionalexpenses-value'),
+    monthsavingsValue = document.querySelector('.monthsavings-value'),
+    yearsavingsValue = document.querySelector('.yearsavings-value'),
+    expensesItem = document.querySelectorALL('.expenses-item'),
+    expensesItemBtn = document.getElementsByTagName('expenses-item-btn'),
+    countBudgetBtn = document.getElementsByTagName('count-budget-btn'),
+    optionalexpensesItem = document.querySelectorALL('.optionalexpenses-item'),
+    chooseIncomeLabel = document.querySelector('.choose-income-label'),
+    checkSavings = document.querySelector('.checksavings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    year = document.querySelector('.year'),
+    month = document.querySelector('.month'),
+    day = document.querySelector('.day');
+ 
+
+    
+
+    
