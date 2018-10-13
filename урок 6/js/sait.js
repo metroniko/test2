774c0520bf2f572c516dfbad4b4dd5c9b1661d58
Kolya <murzinov.nikolay@yandex.ru>
2018-10-13 12:44:36 +0300

    13 morning

--- урок 6/js/sait.js
+++ урок 6/js/sait.js
@@ -25,6 +25,6 @@ let menuItem = document.querySelectorAll('.menu-item'),
 
     menu.insertBefore(menuItem[2], menuItem[1]);
  
-    document.body.style.background = 'url(https://webattach.mail.yandex.net/message_part_real/apple_true.jpg?exif_rotate=y&no_disposition=y&name=apple_true.jpg&sid=67wQ6Hcc0wl5XzJNW8gmCiyPb6QV%2FpbNUstjf3w8iipggG2%2FkkhbKBNdAp%2FACAJjikV3zJwPy0WKl*6Qm2n7VZTfZGuIyQs*Gsqk5m9pQnYQ%2FIDLEw6nbM0Ey3*DUihb)';
+    document.body.style.background = 'url(img/apple_true.jpg)';
 
   
