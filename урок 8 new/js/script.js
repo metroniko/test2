bf8e3111ce541c7ae5ce64c3755870aa24de38f4
Kolya <murzinov.nikolay@yandex.ru>
2018-10-17 20:38:27 +0300

    коммит за 17

---
+++ урок 8 new/js/script.js
@@ -0,0 +1,86 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+        hideTabContent(1);
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+       
+    }
+
+    info.addEventListener('click', function(event) {
+        console.log("здесь");
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i <tab.length; i++) {
+                if(target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+    // таймер
+    let deadline = '2018-10-21';
+    //рассчёт времемени до дедлайна
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime)-Date.parse(new Date),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor(t/(1000*60*60));
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        if (hours < 10) {
+            hours = '0' + hours;
+        }
+        return {
+            'total' : t,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours
+        };
+    }
+    //делает динамику
+    function setClock (id, endtime) {//получение элементов со страницы
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes =  timer.querySelector('.minutes'),
+            seconds =  timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {//добавление времени на страницу
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+            if (t.total <= 0) {
+                clearInterval (timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+
+            }
+        }    
+    }
+
+    setClock ('timer', deadline);
+});
--- урок 8/js/script.js
+++ урок 8/js/script.js
@@ -35,5 +35,52 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+    // таймер
+    let deadline = '2018-10-21';
+    //рассчёт времемени до дедлайна
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime)-Date.parse(new Date),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor(t/(1000*60*60));
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        if (hours < 10) {
+            hours = '0' + hours;
+        }
+        return {
+            'total' : t,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours
+        };
+    }
+    //делает динамику
+    function setClock (id, endtime) {//получение элементов со страницы
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes =  timer.querySelector('.minutes'),
+            seconds =  timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
 
+        function updateClock() {//добавление времени на страницу
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+            if (t.total <= 0) {
+                clearInterval (timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+
+            }
+        }    
+    }
+
+    setClock ('timer', deadline);
 });
