7f066c2d24f1ab43548dc1a2d5dd331abaf6c8fe
Kolya <murzinov.nikolay@yandex.ru>
2018-10-18 22:10:12 +0300

    18 вечер

--- урок 9/script1.js
+++ урок 9/script1.js
@@ -1,11 +1,10 @@
-
 let age = document.getElementById('age');
 console.log(age);
+let Age = {
+    value:age.value
+};
  
 function showUser(surname, name) {   
     alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);        
 } 
-let Age = {
-    value: age.value
-};
 showUser.apply(Age, ['murza','nin']);
